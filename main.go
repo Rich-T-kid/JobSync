56657b9c13f9903655107adb6aa55f9b5a7c5082
rich <richiebbaah@gmail.com>
2024-02-17 14:27:37 -0500

    Finished loading the basic html files in static. homepage, root, login, signup, forgot password

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import ("fmt")
+import ("fmt"
+	"io"
+//	"os"
+//	"errors"
+	"net/http")
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("root")
+	io.WriteString(w, "This is my website!\n")
+}
 
 
 func main(){
-	fmt.Println("lets go")
+	http.HandleFunc("/" , getRoot)
+	fmt.Print("lets start")
+	server := http.ListenAndServe(":8080",nil)
+	if server != nil{
+		fmt.Println("error occured" , server)
+		
+	}else{
+		fmt.Println("starting on port 8080" , server)
+		
+	}
 
+	
 }
